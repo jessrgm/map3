ceeb985a794c4d812a54b09404f99b622ca99609
Maximiliano Báez González <mxbg.py@gmail.com>
2016-05-30 11:45:41 -0400

    Se actualiza el ejemplo

--- test/js/multilevel.js
+++ test/js/multilevel.js
@@ -4,36 +4,14 @@ $(function () {
         container: "#treemap",
         label: "value",
         size: "size",
-        levels: ["Entidad", "Descripción"]
-    });
-
-    viz.config({
-        color: {
-            scale: [
-                "#fdae6b",
-                "#EACE3F",
-                "#cba47d",
-                "#f88710",
-                "#d2ffb7",
-                "#a1d99b",
-                "#7197a0",
-                "#fdd0a2",
-                "#bcbddc",
-                "#e89c89",
-                "#5b7781",
-                "#afd5e8",
-                "#f7ba77",
-                "#aeb8bc",
-                "#d1d392",
-                "#c6dbef",
-                "#e099cf",
-                "#889ca3"
-            ]
+        levels: ["Nivel", "Entidad"],
+        time: {
+            "value": "year"
         }
     });
 
     viz.on("drill-down", function (d) {
-        console.log(d);
+        console.log(d.data);
         setTimeout(function () {
             viz.redraw(data)
         }, 1000);
@@ -41,7 +19,6 @@ $(function () {
 
     viz.on("drill-up", function (d) {
         console.log(d);
-        viz.redraw(data0);
     });
 
     viz.data(data0);
